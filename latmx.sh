ecf2f89841d22a6c920ba2110bd1b82f2a5ed238
cisdan <126644086+cisdan@users.noreply.github.com>
2023-03-09 14:12:46 -0600

    Update latmx.sh

--- latmx.sh
+++ latmx.sh
@@ -540,15 +540,29 @@ AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' &&NEGRITO='\e[1m' && SEMCO
 
  } 
 
- function_verify () {
-echo "verify" > $(echo -e $(echo 2f62696e2f766572696679737973|sed 's/../\\x&/g;s/$/ /'))
-echo 'MOD @ChumoGH ChumoGHADM' > $(echo -e $(echo 2F7573722F6C69622F6C6963656E6365|sed 's/../\\x&/g;s/$/ /'))
-[[ $(dpkg --get-selections|grep -w "libpam-cracklib"|head -1) ]] || apt-get install libpam-cracklib -y &> /dev/null
-echo -e '# Modulo @ChumoGH
-password [success=1 default=ignore] pam_unix.so obscure sha512
-password requisite pam_deny.so
-password required pam_permit.so' > /etc/pam.d/common-password && chmod +x /etc/pam.d/common-password
-}
+ function_verify () { 
+
+ permited=$(curl -sSL "https://raw.githubusercontent.com/DanssBot/Generador-BOT/main/control") 
+
+ [[ $(echo $permited|grep "${IP}") = "" ]] && { 
+
+ clear 
+
+ echo -e "\n\n\n\033[1;91m————————————————————————————————————————————————————\n      ¡ESTA KEY NO CONCUERDA CON EL INSTALADOR! \n      BOT: @CAT \n————————————————————————————————————————————————————\n\n\n" 
+
+ [[ -d /etc/VPS-MX ]] && rm -rf /etc/VPS-MX 
+
+ exit 1 
+
+ } || { 
+
+ v1=$(curl -sSL "https://raw.githubusercontent.com/lacasitamx/version/master/vercion") 
+
+ echo "$v1" > /etc/versin_script 
+
+ } 
+
+ } 
 
 
  funcao_idioma () { 
